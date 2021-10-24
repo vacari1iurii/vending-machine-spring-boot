99ffa908c87f2fe45e52607ac3765682665cfd0a
Iurii Vacari <vacari1iurii@gmail.com>
2021-10-25 00:32:03 +0300

    log: logging added

--- src/main/java/com/companyname/vending_machine/controller/BuyerController.java
+++ src/main/java/com/companyname/vending_machine/controller/BuyerController.java
@@ -10,11 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.companyname.vending_machine.model.dto.BuyRequestDTO;
+import com.companyname.vending_machine.model.dto.ResponseBuyDTO;
 import com.companyname.vending_machine.model.dto.ResponseVendingMachineItemDTO;
 import com.companyname.vending_machine.service.VendingMachineService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/buyer")
+@Slf4j
 public class BuyerController {
     private final VendingMachineService service;
 
@@ -24,12 +28,14 @@ public class BuyerController {
 
     @GetMapping("/getAll")
     public ResponseEntity<List<ResponseVendingMachineItemDTO>> showItemList() {
+        log.debug("BuyerController.showItemList is called.");
         return ResponseEntity.ok(service.getAllItems());
     }
 
     @PostMapping("/buy")
     public ResponseEntity<String> buy(@RequestBody BuyRequestDTO requestDTO) {
-        String result = service.buy(requestDTO);
-        return ResponseEntity.ok(result);
+        log.debug("BuyerController.buy is called.");
+        ResponseBuyDTO result = service.buy(requestDTO);
+        return ResponseEntity.ok(String.format("Purchase completed successfully. Your balance is %s.", result.getBalance()));
     }
 }
